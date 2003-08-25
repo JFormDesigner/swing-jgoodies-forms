fc3c67020afa0d96b59085c78710b91a095e0b79
karsten <karsten@106c21d4-d539-c70d-aa97-d2a26c09264f>
2003-08-25 19:07:39 +0000

    Added again

---
+++ src/tutorial/com/jgoodies/forms/tutorial/basics/AlignmentExample.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2003 JGoodies Karsten Lentzsch. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  o Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *     
+ *  o Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution. 
+ *     
+ *  o Neither the name of JGoodies Karsten Lentzsch nor the names of 
+ *    its contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission. 
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+package com.jgoodies.forms.tutorial.basics;
+
+import javax.swing.*;
+
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+/**
+ * Demonstrates the different FormLayout alignments.
+ *
+ * @author	Karsten Lentzsch
+ */
+public final class AlignmentExample {
+
+    
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.jgoodies.plaf.plastic.PlasticXPLookAndFeel");
+        } catch (Exception e) {}
+        JFrame frame = new JFrame();
+        frame.setTitle("Forms Tutorial :: Alignments");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        JComponent panel = new AlignmentExample().buildPanel();
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.show();
+    }
+
+
+    public JComponent buildPanel() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.putClientProperty("jgoodies.noContentBorder", Boolean.TRUE);
+
+        tabbedPane.add("Horizontal", buildHorizontalButtons());
+        tabbedPane.add("Vertical",   buildVerticalButtons());
+        return tabbedPane;
+    }
+    
+    
+    private JComponent buildHorizontalButtons() {
+        FormLayout layout = new FormLayout(
+            "left:pref, 25px, center:pref, 25px, right:pref, 25px, fill:pref",   
+            "pref, 12px, pref, 4px, pref, 4px, pref, 4px, pref, 4px, pref"); 
+            
+        // Create a panel that uses the layout.
+        JPanel panel = new JPanel(layout);
+
+        // Set a default border.
+        panel.setBorder(Borders.DIALOG_BORDER);
+        
+        // Create a reusable CellConstraints instance.
+        CellConstraints cc = new CellConstraints();
+
+        // Add components to the panel.
+        panel.add(new JLabel("Left"),     cc.xy(1,  1));
+        panel.add(new JButton("Name"),    cc.xy(1,  3));
+        panel.add(new JButton("Phone"),   cc.xy(1,  5));
+        panel.add(new JButton("Fax"),     cc.xy(1,  7));
+        panel.add(new JButton("Email"),   cc.xy(1,  9));
+        panel.add(new JButton("Address"), cc.xy(1, 11));
+
+        panel.add(new JLabel("Center"),   cc.xy(3,  1));
+        panel.add(new JButton("Name"),    cc.xy(3,  3));
+        panel.add(new JButton("Phone"),   cc.xy(3,  5));
+        panel.add(new JButton("Fax"),     cc.xy(3,  7));
+        panel.add(new JButton("Email"),   cc.xy(3,  9));
+        panel.add(new JButton("Address"), cc.xy(3, 11));
+
+        panel.add(new JLabel("Right"),    cc.xy(5,  1));
+        panel.add(new JButton("Name"),    cc.xy(5,  3));
+        panel.add(new JButton("Phone"),   cc.xy(5,  5));
+        panel.add(new JButton("Fax"),     cc.xy(5,  7));
+        panel.add(new JButton("Email"),   cc.xy(5,  9));
+        panel.add(new JButton("Address"), cc.xy(5, 11));
+
+        panel.add(new JLabel("Fill"),     cc.xy(7,  1));
+        panel.add(new JButton("Name"),    cc.xy(7,  3));
+        panel.add(new JButton("Phone"),   cc.xy(7,  5));
+        panel.add(new JButton("Fax"),     cc.xy(7,  7));
+        panel.add(new JButton("Email"),   cc.xy(7,  9));
+        panel.add(new JButton("Address"), cc.xy(7, 11));
+
+        return panel;
+    }
+    
+    
+    private JComponent buildVerticalButtons() {
+        FormLayout layout = new FormLayout(
+            "pref, 12px, pref, 6px, pref, 6px, pref",   
+            "top:pref, 15px, center:pref, 15px, bottom:pref, 15px, fill:pref"); 
+            
+        // Create a panel that uses the layout.
+        JPanel panel = new JPanel(layout);
+
+        // Set a default border.
+        panel.setBorder(Borders.DIALOG_BORDER);
+        
+        // Create a reusable CellConstraints instance.
+        CellConstraints cc = new CellConstraints();
+
+        // Add components to the panel.
+        panel.add(new JLabel("Top"),      cc.xy(1,  1));
+        panel.add(createSmallButton(),    cc.xy(3,  1));
+        panel.add(createMediumButton(),   cc.xy(5,  1));
+        panel.add(createLargeButton(),    cc.xy(7,  1));
+
+        panel.add(new JLabel("Center"),   cc.xy(1,  3));
+        panel.add(createSmallButton(),    cc.xy(3,  3));
+        panel.add(createMediumButton(),   cc.xy(5,  3));
+        panel.add(createLargeButton(),    cc.xy(7,  3));
+
+        panel.add(new JLabel("Bottom"),   cc.xy(1,  5));
+        panel.add(createSmallButton(),    cc.xy(3,  5));
+        panel.add(createMediumButton(),   cc.xy(5,  5));
+        panel.add(createLargeButton(),    cc.xy(7,  5));
+
+        panel.add(new JLabel("Fill"),     cc.xy(1,  7));
+        panel.add(createSmallButton(),    cc.xy(3,  7));
+        panel.add(createMediumButton(),   cc.xy(5,  7));
+        panel.add(createLargeButton(),    cc.xy(7,  7));
+
+        return panel;
+    }
+    
+    private JButton createSmallButton() {
+        return new JButton("<html>One</html>");
+    }
+    
+    private JButton createMediumButton() {
+        return new JButton("<html>One<br>Two</html>");
+    }
+    
+    private JButton createLargeButton() {
+        return new JButton("<html>One<br>Two<br>Three</html>");
+    }
+    
+    
+}
+
---
+++ src/tutorial/com/jgoodies/forms/tutorial/basics/GroupingExample.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2003 JGoodies Karsten Lentzsch. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  o Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *     
+ *  o Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution. 
+ *     
+ *  o Neither the name of JGoodies Karsten Lentzsch nor the names of 
+ *    its contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission. 
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+package com.jgoodies.forms.tutorial.basics;
+
+import javax.swing.*;
+
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+/**
+ * Demonstrates how columns and rows can be grouped in FormLayout.
+ *
+ * @author	Karsten Lentzsch
+ */
+public final class GroupingExample {
+    
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.jgoodies.plaf.plastic.PlasticXPLookAndFeel");
+        } catch (Exception e) {}
+        JFrame frame = new JFrame();
+        frame.setTitle("Forms Tutorial :: Grouping");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        JComponent panel = new GroupingExample().buildPanel();
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.show();
+    }
+
+
+    public JComponent buildPanel() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.putClientProperty("jgoodies.noContentBorder", Boolean.TRUE);
+
+        tabbedPane.add("Ungrouped Bar",   buildWizardBar(false));
+        tabbedPane.add("Grouped Bar",     buildWizardBar(true));
+        tabbedPane.add("Ungrouped Rows",  buildEditorPanel(false));
+        tabbedPane.add("Grouped Rows",    buildEditorPanel(true));
+        return tabbedPane;
+    }
+    
+    
+    private JComponent buildWizardBar(boolean grouped) {
+        FormLayout layout = new FormLayout(
+            "pref, 6px:grow, pref, pref, 12px, pref, 6px, pref",   
+            "pref");
+        if (grouped) { 
+            layout.setColumnGroups(new int[][]{{1, 3, 4, 6, 8}});
+        }
+        JPanel panel = new JPanel(layout);
+        panel.setBorder(Borders.DIALOG_BORDER);
+        CellConstraints cc = new CellConstraints();
+
+        panel.add(createNarrowButton("Hilfe"),      cc.xy(1, 1));
+        panel.add(createNarrowButton("< Zurück"),   cc.xy(3, 1));
+        panel.add(createNarrowButton("Vor >"),      cc.xy(4, 1));
+        panel.add(createNarrowButton("Beenden"),    cc.xy(6, 1));
+        panel.add(createNarrowButton("Abbrechen"),  cc.xy(8, 1));
+        
+        return panel;
+    }
+    
+    
+    private JComponent buildEditorPanel(boolean grouped) {
+        FormLayout layout = new FormLayout(
+                "pref, 4dlu, 35dlu, 2dlu, 35dlu, 2dlu, 35dlu, 2dlu, 35dlu",
+                "p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p");
+        if (grouped) {
+            layout.setRowGroups(new int[][] { { 1, 3, 5, 7, 9, 11, 13, 15, 17 } });
+        }  
+            
+        JPanel panel = new JPanel(layout);
+        panel.setBorder(Borders.DIALOG_BORDER);
+        CellConstraints cc = new CellConstraints();
+
+        panel.add(new JLabel("File number:"),       cc.xy  (1,  1));
+        panel.add(new JTextField(),                 cc.xywh(3,  1, 7, 1));
+        panel.add(new JLabel("BL/MBL number:"),     cc.xy  (1,  3));
+        panel.add(new JTextField(),                 cc.xy  (3,  3));
+        panel.add(new JTextField(),                 cc.xy  (5,  3));
+        panel.add(new JLabel("Entry date:"),        cc.xy  (1,  5));
+        panel.add(new JTextField(),                 cc.xy  (3,  5));
+        panel.add(new JLabel("RFQ number:"),        cc.xy  (1,  7));
+        panel.add(new JTextField(),                 cc.xywh(3,  7, 7, 1));
+        panel.add(new JLabel("Goods:"),             cc.xy  (1,  9));
+        panel.add(new JCheckBox("Dangerous"),       cc.xywh(3,  9, 7, 1));
+        panel.add(new JLabel("Shipper:"),           cc.xy  (1, 11));
+        panel.add(new JTextField(),                 cc.xywh(3, 11, 7, 1));
+        panel.add(new JLabel("Customer:"),          cc.xy  (1, 13));
+        panel.add(new JTextField(),                 cc.xywh(3, 13, 5, 1));
+        panel.add(new JButton("..."),               cc.xy  (9, 13));
+        panel.add(new JLabel("Port of loading:"),   cc.xy  (1, 15));
+        panel.add(new JTextField(),                 cc.xywh(3, 15, 7, 1));
+        panel.add(new JLabel("Destination:"),       cc.xy  (1, 17));
+        panel.add(new JTextField(),                 cc.xywh(3, 17, 7, 1));
+        
+        return panel;
+    }
+    
+    
+    // Component Creation *****************************************************
+    
+    private JButton createNarrowButton(String text) {
+        JButton button = new JButton(text);
+        button.putClientProperty("jgoodies.isNarrow", Boolean.TRUE);
+        return button;
+    }
+    
+    
+}
+
