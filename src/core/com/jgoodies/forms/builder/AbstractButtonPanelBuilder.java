db748d3656510414ab5566a228f26eeb57fc314f
karsten <karsten@106c21d4-d539-c70d-aa97-d2a26c09264f>
2008-01-16 16:57:20 +0000

    Improved docs

--- src/core/com/jgoodies/forms/builder/AbstractButtonPanelBuilder.java
+++ src/core/com/jgoodies/forms/builder/AbstractButtonPanelBuilder.java
@@ -45,13 +45,17 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 
 /**
- * The abstract superclass for {@link ButtonBarBuilder2} and
- * {@code ButtonStackBuilder2}. Provides a cell cursor for traverse
- * the button bar/stack while components are added. Also, it offers
- * convenience methods to append logical columns and rows.
+ * The abstract superclass for {@link ButtonBarBuilder2}. 
+ * Provides a cell cursor for traversing
+ * the button bar/stack while components are added. It also offers
+ * convenience methods to append logical columns and rows.<p>
+ * 
+ * TODO: Mention the ButtonStackBuilder2 subclass as soon as it is available.
  *
  * @author Karsten Lentzsch
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
+ * 
+ * @since 1.2
  */
 public abstract class AbstractButtonPanelBuilder {
 
