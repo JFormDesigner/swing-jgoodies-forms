f1c3b0c7e2d8774ed89fd393aa59eea6d25a47c9
karsten <karsten@106c21d4-d539-c70d-aa97-d2a26c09264f>
2012-04-18 10:45:40 +0000

    Added docs and an invalidation mechanism

--- src/main/java/com/jgoodies/forms/builder/ListViewBuilder.java
+++ src/main/java/com/jgoodies/forms/builder/ListViewBuilder.java
@@ -41,7 +41,7 @@ public final class ListViewBuilder {
     private JComponent listExtrasView;
     private JComponent detailsView;
     
-    private Border     border;
+    private Border border;
     private int filterViewMinimumWidthDLU = 100;
     private int listViewMinimumHeightDLU  = 100;
 
@@ -53,11 +53,23 @@ public final class ListViewBuilder {
     
     // Instance Creation ******************************************************
     
+    /**
+     * Constructs a ListViewBuilder using the AbstractBuilder's
+     * default component factory. The factory is required by
+     * {@link #setLabel(String)} and {@link #setHeaderLabel(String)}.
+     */
     public ListViewBuilder() {
     	this(AbstractBuilder.getDefaultComponentFactory());
     }
     
     
+    /**
+     * Constructs a ListViewBuilder using the given component factory. 
+     * The factory is required by
+     * {@link #setLabel(String)} and {@link #setHeaderLabel(String)}.
+     * 
+     * @param factory   the component factory used to create labels and headers
+     */
     public ListViewBuilder(ComponentFactory factory) {
     	this.factory = factory;
     }
@@ -65,53 +77,134 @@ public final class ListViewBuilder {
 
     // API ********************************************************************
 
+    /**
+     * Sets the mandatory label view. Useful to set a bound label that updates
+     * its text when the list content changes, for example to provide the
+     * number of list elements.
+     * 
+     * @param labelView   the component that shall label the list view,
+     *    often a bound label
+     */
     public void setLabelView(JComponent labelView) {
         this.labelView = labelView;
+        invalidatePanel();
     }
 
 
-    public void setLabel(String markedLabel) {
-        setLabelView(factory.createLabel(markedLabel));
+    /**
+     * Creates a plain label for the given marked text and sets it as label view.
+     * Equivalent to:
+     * <pre>
+     * setLabelView(aComponentFactory.createLabel(markedText));
+     * </pre>
+     * 
+     * @param markedText   the label's text, may contain a mnemonic marker
+     */
+    public void setLabel(String markedText) {
+        setLabelView(factory.createLabel(markedText));
     }
 
 
-    public void setHeaderLabel(String markedLabel) {
-        setLabelView(factory.createHeaderLabel(markedLabel));
+    /**
+     * Creates a header label for the given marked text and sets it as label view.
+     * Equivalent to:
+     * <pre>
+     * setLabelView(aComponentFactory.createHeaderLabel(markedText));
+     * </pre>
+     * 
+     * @param markedText   the label's text, may contain a mnemonic marker
+     */
+    public void setHeaderLabel(String markedText) {
+        setLabelView(factory.createHeaderLabel(markedText));
     }
     
     
+    /**
+     * Sets an optional view that will be placed in the upper right corner
+     * of the built list view panel. This can be a search field, a panel
+     * with filtering check boxes ("Only valid items"), etc.
+     * 
+     * @param filterView    the view to be added.
+     */
     public void setFilterView(JComponent filterView) {
         this.filterView = filterView;
+        invalidatePanel();
     }
 
 
+    /**
+     * Changes the filter view's default minimum width of 100dlu.
+     * 
+     * @param minimumWidthtDLU   the width in DLU to be used
+     * 
+     * @throws IllegalArgumentException if {@code minimumWidthDLU} is not positive
+     */
     public void setFilterViewMinimumWidth(int minimumWidthtDLU) {
     	checkArgument(minimumWidthtDLU > 0, "The filter view minimum width must be positive.");
     	this.listViewMinimumHeightDLU = minimumWidthtDLU;
+        invalidatePanel();
     }
 
 
+    /**
+     * Wraps the given component and sets it as the the mandatory list view.
+     * 
+     * @param listView   the component to be used as scrollable list view
+     * 
+     * @throws NullPointerException if {@code listView} is {@code null}
+     */
     public void setListView(JComponent listView) {
-        setListView(new JScrollPane(listView));
+        setListView(new JScrollPane(checkNotNull(listView, "The list view must not be null.")));
     }
     
     
+    /**
+     * Sets the given scrollable content as mandatory list view.
+     * 
+     * @param listView  the scroll pane that contains the list view
+     * 
+     * @throws NullPointerException if {@code listView} is {@code null}
+     */
     public void setListView(JScrollPane listView) {
-    	this.listView = listView;
+    	this.listView = checkNotNull(listView, "The list view must not be null.");
+        invalidatePanel();
     }
     
     
+    /**
+     * Changes the list view's default minimum height of 100dlu.
+     * 
+     * @param minimumHeightDLU   the height in DLU to be used
+     * 
+     * @throws IllegalArgumentException if {@code minimumHeightDLU} is not positive
+     */
     public void setListViewMinimumHeight(int minimumHeightDLU) {
     	checkArgument(minimumHeightDLU > 0, "The list view minimum height must be positive.");
     	this.listViewMinimumHeightDLU = minimumHeightDLU;
+        invalidatePanel();
     }
 
 
+    /**
+     * Sets an optional list bar - often button bar - that will be located
+     * in the lower right corner of the list view.
+     * 
+     * @param listBarView   the component to set
+     */
     public void setListBarView(JComponent listBarView) {
         this.listBarView = listBarView;
+        invalidatePanel();
     }
 
 
+    /**
+     * Builds a button bar using the given buttons and sets it as list bar.
+     * 
+     * @param buttons    the buttons in the list bar
+     * 
+     * @throws NullPointerException if {@code buttons} is {@code null}
+     * @throws IllegalArgumentException if no buttons are provided
+     */
     public void setListBar(JButton... buttons) {
         checkNotNull(buttons, "The button array must not be null.");
         checkArgument(buttons.length > 0, "You must provide at least one button.");
@@ -133,21 +226,47 @@ public final class ListViewBuilder {
     }
 
 
+    /**
+     * Sets an optional view that is located in the lower right corner
+     * of the list view, aligned with the list bar.
+     * 
+     * @param listExtrasView    the component to set
+     */
     public void setListExtrasView(JComponent listExtrasView) {
         this.listExtrasView = listExtrasView;
+        invalidatePanel();
     }
 
 
+    /**
+     * Sets an optional details view that is located under the list view.
+     * Often this is the details view or preview of a master-details view.
+     * 
+     * @param detailsView    the component to set
+     */
     public void setDetailsView(JComponent detailsView) {
         this.detailsView = detailsView;
+        invalidatePanel();
     }
 
 
+    /**
+     * Sets an optional border that surrounds the list view including
+     * the label and details.
+     * 
+     * @param border   the border to set
+     */
     public void setBorder(Border border) {
         this.border = border;
+        invalidatePanel();
     }
 
 
+    /**
+     * Lazily builds and returns the list view panel.
+     * 
+     * @return the built panel
+     */
     public JComponent getPanel() {
         if (panel == null) {
             panel = buildPanel();
@@ -157,6 +276,10 @@ public final class ListViewBuilder {
 
 
     // Implementation *********************************************************
+    
+    private void invalidatePanel() {
+    	panel = null;
+    }
 
     private JComponent buildPanel() {
     	checkNotNull(labelView, "The label must be set before #getPanel is invoked.");
