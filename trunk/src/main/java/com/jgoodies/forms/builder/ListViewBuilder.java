bf7c2b79c9506dc5ac7da48d03cd35fb42ecf166
karsten <karsten@106c21d4-d539-c70d-aa97-d2a26c09264f>
2012-05-04 07:20:23 +0000

    Changed docs

--- trunk/src/main/java/com/jgoodies/forms/builder/ListViewBuilder.java
+++ trunk/src/main/java/com/jgoodies/forms/builder/ListViewBuilder.java
@@ -1,9 +1,31 @@
 /*
- * Copyright (c) 2006-2012 JGoodies Karsten Lentzsch. All Rights Reserved.
+ * Copyright (c) 2002-2012 JGoodies Karsten Lentzsch. All Rights Reserved.
  *
- * This software is the proprietary information of JGoodies Karsten Lentzsch.
- * Use is subject to license terms.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of JGoodies Karsten Lentzsch nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.jgoodies.forms.builder;
@@ -32,6 +54,8 @@ import com.jgoodies.forms.layout.FormLayout;
  *
  * @author  Karsten Lentzsch
  * @version $Revision: 1.2 $
+ * 
+ * @since 1.6
  */
 public final class ListViewBuilder {
 	
@@ -181,7 +205,14 @@ public final class ListViewBuilder {
      * that is filled by the list view; the height is determined 
      * by the list view's preferred height, but a minimum of 100dlu is ensured.
      * The list view grows vertically, if the container gets more vertical 
-     * space.
+     * space.<p>
+     * 
+     * <strong>Examples:</strong>
+     * <pre>
+     * builder.setListViewRowSpec("fill:100dlu");  // fixed height
+     * builder.setListViewRowSpec("f:100dlu:g");   // fixed start height, grows
+     * builder.setListViewRowSpec("f:p");          // no minimum height
+     * </pre>
      * 
      * @param rowSpec   specifies the vertical layout of the list view
      * 
@@ -195,8 +226,10 @@ public final class ListViewBuilder {
 
 
     /**
-     * Sets an optional list bar - often button bar - that will be located
-     * in the lower right corner of the list view.
+     * Sets an optional list bar - often a button bar - 
+     * that will be located in the lower left corner of the list view. 
+     * If the list bar view consists only of buttons, 
+     * use {@link #setListBar(JButton...)} instead.
      * 
      * @param listBarView   the component to set
      */
