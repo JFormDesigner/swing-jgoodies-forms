2aeb3ef45e648aa55f6cd58af3ef57197bebb5d4
karsten <karsten@106c21d4-d539-c70d-aa97-d2a26c09264f>
2012-11-14 18:47:08 +0000

    Fixed Forms#centered, #horizontal, #vertical; completed JavaDocs

--- src/main/java/com/jgoodies/forms/factories/Forms.java
+++ src/main/java/com/jgoodies/forms/factories/Forms.java
@@ -52,6 +52,8 @@ import com.jgoodies.forms.layout.FormLayout;
  *
  * @author Karsten Lentzsch
  * @version $Revision: 1.1 $
+ * 
+ * @since 1.6
  */
 public final class Forms {
 
@@ -62,6 +64,24 @@ public final class Forms {
 
 	// Public API *************************************************************
 
+	/**
+	 * Creates and returns a panel where {@code component} is laid out
+	 * using a FormLayout with the given column and row specifications.
+     * <blockquote><pre>
+     * Forms.single(  "left:150dlu",      "c:p",     component);
+     * Forms.single(  "fill:150dlu:grow", "f:20dlu", component);
+     * Forms.single("center:150dlu",      "b:p:g",   component);
+     * </pre></blockquote>
+	 * 
+	 * @param columnSpec    a FormLayout column specification for a single column
+	 * @param rowSpec       a FormLayout row specification for a single row
+	 * @param component     the component to lay out
+	 * @return the built panel
+	 * @throws NullPointerException if {@code columnSpec}, {@code rowSpec},
+	 *   or {@component} is {@code null}
+	 * @throws IllegalArgumentException if {@code columnSpec} or {@code rowSpec}
+	 *   is empty or whitespace
+	 */
 	public static JComponent single(String columnSpec, String rowSpec,
 			JComponent component) {
 		checkNotBlank(columnSpec, MUST_NOT_BE_BLANK, "column specification");
@@ -74,11 +94,36 @@ public final class Forms {
 	}
 
 
+	/**
+	 * Creates and returns a panel where {@code component} is centered.
+	 * The panel grows both horizontally and vertically as its container grows.
+     * <blockquote><pre>
+     * Forms.centered(anImageLabel);
+     * Forms.centered(anAnimatedPanel);
+     * </pre></blockquote>
+	 * 
+	 * @param component     the component to center
+	 * @return the built panel
+	 * @throws NullPointerException if {@component} is {@code null}
+	 */
 	public static JComponent centered(JComponent component) {
-		return single("fill:pref:grow", "f:p:g", component);
+		return single("center:pref:grow", "c:p:g", component);
 	}
 
 
+	/**
+	 * Creates and returns a panel where {@code component} is surrounded
+	 * by the given border.
+     * <blockquote><pre>
+     * Forms.border(Borders.DLU14, aComponent);
+     * Forms.border(new LineBorder(Color.GRAY), aComponent);
+     * </pre></blockquote>
+	 * 
+	 * @param border        the border used to surround the component
+	 * @param component     the component to wrap
+	 * @return the built panel
+	 * @throws NullPointerException if {@code border}  or {@component} is {@code null}
+	 */
 	public static JComponent border(Border border, JComponent component) {
 		JComponent container = single("fill:pref", "f:p", component);
 		container.setBorder(border);
@@ -86,29 +131,102 @@ public final class Forms {
 	}
 
 
+	/**
+	 * Creates and returns a panel where {@code component} is surrounded
+	 * by an empty border as described by {@code emptyBorderSpec}.
+     * <blockquote><pre>
+     * Forms.border("4dlu, 0, 4dlu, 8dlu", aComponent);
+     * Forms.border("10px 4px, 10px, 4px", aComponent);
+     * </pre></blockquote>
+	 * 
+	 * @param component     the component to wrap
+	 * @return the built panel
+	 * @throws NullPointerException if {@code emptyBorderSpec} or {@component} is {@code null}
+	 */
 	public static JComponent border(String emptyBorderSpec, JComponent component) {
 		return border(Borders.createEmptyBorder(emptyBorderSpec), component);
 	}
 
 
+    /**
+     * Builds and returns a panel where the given components are laid out
+     * horizontally separated by gaps as described by the given
+     * FormLayout gap (column) specification.
+     * <blockquote><pre>
+     * Forms.horizontal("4dlu",  component1, component2);
+     * Forms.horizontal("$rgap", component1, component2, component3);
+     * Forms.horizontal("0",     component1, component2, component3);
+     * </pre></blockquote>
+     *
+     * @param gapColSpec    describes the horizontal gap between the components
+     * @param components    the components to be laid out
+     * @return the built panel
+     * @throws NullPointerException if {@code components} is {@code null}
+     * @throws IllegalArgumentException if {@code components} is empty
+     */
 	public static JComponent horizontal(String gapColSpec, JComponent... components) {
 		checkNotBlank(gapColSpec, MUST_NOT_BE_BLANK, "gap column specification");
 		checkNotNull(components, MUST_NOT_BE_NULL, "component array");
-		FormLayout layout = new FormLayout();
-		PanelBuilder builder = new PanelBuilder(layout);
+        checkArgument(components.length > 1, "You must provide more than one component.");
+        FormLayout layout = new FormLayout(
+        		components.length - 1 + "*(pref, " + gapColSpec + "), pref",
+                "p");
+        PanelBuilder builder = new PanelBuilder(layout);
+        int column = 1;
+        for (JComponent component : components) {
+            builder.add(component, CC.xy(column, 1));
+            column += 2;
+        }
 		return builder.build();
 	}
 
 
+    /**
+     * Builds and returns a panel where the given components are laid out
+     * vertically separated by gaps as described by the given
+     * FormLayout gap (row) specification.
+     * <blockquote><pre>
+     * Forms.vertical("4dlu",  component1, component2);
+     * Forms.vertical("$rgap", component1, component2, component3);
+     * Forms.vertical("0",     component1, component2, component3);
+     * </pre></blockquote>
+     *
+     * @param gapRowSpec    describes the vertical gap between the components
+     * @param components    the components to be laid out
+     * @return the built panel
+     * @throws NullPointerException if {@code components} is {@code null}
+     * @throws IllegalArgumentException if {@code components} is empty
+     */
 	public static JComponent vertical(String gapRowSpec, JComponent... components) {
 		checkNotBlank(gapRowSpec, MUST_NOT_BE_BLANK, "gap row specification");
 		checkNotNull(components, MUST_NOT_BE_NULL, "component array");
-		FormLayout layout = new FormLayout();
-		PanelBuilder builder = new PanelBuilder(layout);
+        checkArgument(components.length > 1, "You must provide more than one component.");
+        FormLayout layout = new FormLayout(
+        		"pref",
+        		components.length - 1 + "*(p, " + gapRowSpec + "), p");
+        PanelBuilder builder = new PanelBuilder(layout);
+        int row = 1;
+        for (JComponent component : components) {
+            builder.add(component, CC.xy(row, 1));
+            row += 2;
+        }
 		return builder.build();
 	}
 
 
+	/**
+	 * Creates and returns a panel where the given buttons are laid out
+	 * using a ButtonBarBuilder. Equivalent to:
+     * <blockquote><pre>
+     * new ButtonBarBuilder().addButton(buttons).build();
+     * </pre></blockquote>
+	 * 
+	 * @param buttons  the buttons to add to the button bar
+	 * @return the built button bar
+     * @throws NullPointerException if {@code buttons} is {@code null}
+     * @throws IllegalArgumentException if {@code buttons} is empty
+	 * @see ButtonBarBuilder
+	 */
 	public static JComponent buttonBar(JComponent... buttons) {
 		return new ButtonBarBuilder()
 			.addButton(buttons)
@@ -127,6 +245,8 @@ public final class Forms {
      * the selection in the group.
      *
      * @return the built panel
+     * @throws NullPointerException if {@code checkBoxes} is {@code null}
+     * @throws IllegalArgumentException if {@code checkBoxes} is empty
      */
 	public static JComponent checkBoxBar(JCheckBox... checkBoxes) {
 	    return buildGroupedButtonBar(checkBoxes);
@@ -144,6 +264,8 @@ public final class Forms {
      * the selection in the group.
      *
      * @return the built panel
+     * @throws NullPointerException if {@code radioButtons} is {@code null}
+     * @throws IllegalArgumentException if {@code radioButtons} is empty
      */
     public static JComponent radioButtonBar(JRadioButton... radioButtons) {
         return buildGroupedButtonBar(radioButtons);
@@ -165,10 +287,10 @@ public final class Forms {
                 buttons.length - 1 + "*(pref, $rgap), pref",
                 "p");
         PanelBuilder builder = new PanelBuilder(layout);
-        int columnCount = 1;
+        int column = 1;
         for (AbstractButton button : buttons) {
-            builder.add(button, CC.xy(columnCount, 1));
-            columnCount += 2;
+            builder.add(button, CC.xy(column, 1));
+            column += 2;
         }
         FocusTraversalUtilsAccessor.tryToBuildAFocusGroup(buttons);
         return builder.build();
